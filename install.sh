6d14fd9b1f36ccf9f042d283fa3f42f318c7d9cb
Maxime Augier <max@xolus.net>
2017-02-21 00:12:10 +0100

    Make install.sh call pip3 to install game package

--- install.sh
+++ install.sh
@@ -3,23 +3,20 @@
 echo "This is OpenAllumette installator\n\n"
 
 
-DISTRIBUTION=`lsb_release -i | sed 's/Distributor ID:\t//'`
+DISTRIBUTION=$(lsb_release -i | sed 's/Distributor ID:\t//')
+LOCALPATH=~/.allumette
+SRC_URI="https://github.com/FauveNoir/allumette.git"
 
 case $DISTRIBUTION in
 	"Debian")
 		echo "* Installing all needed packages"
-		sudo aptitude install git python3 python3-pip
+		sudo aptitude install git python3 python3-pip || { echo "Cannot pull needed packages"; exit 1; }
 
 		echo "* Installing Pygame with Pip"
-		pip3 install Pygame
+		pip3 install Pygame || { echo "Cannot install pygame"; exit 2; }
 
 		echo "Downloading and inpacking OpenAllumette"
-		git clone https://github.com/FauveNoir/allumette.git ~/.allumette
-		mkdir  ~/.local/bin/
-		chmod +x  ~/.allumette/nim.py
-		ln -s ~/.allumette/nim.py ~/.local/bin/allumette
 
-		echo "\n\nYou can now run OpenAllumette with the command allumette. Good game."
 	;;
 	"ManjaroLinux")
 		echo "* Installing all needed packages"
@@ -35,14 +32,18 @@ case $DISTRIBUTION in
 		pip3 install Pygame
 
 		echo "Downloading and inpacking OpenAllumette"
-		git clone https://github.com/FauveNoir/allumette.git ~/.allumette
-		mkdir  ~/.local/bin/
-		chmod +x  ~/.allumette/nim.py
-		ln -s ~/.allumette/nim.py ~/.local/bin/allumette
 
-		echo "\n\nYou can now run OpenAllumette with the command allumette. Good game."
 	;;
 	*)
 		echo "There is not yet installation for your distribution.\nMaybe are you a volunteer to do it? :)"
+        echo "Trying to install using pip3 anyway"
 	;;
 esac
+
+{ cd ${LOCALPATH}; git pull; } || \
+        git clone ${SRC_URI} ${LOCALPATH} || \
+        { echo "Cannot download source"; exit 3; }
+
+pip3 install --user ~/.allumette && \
+		echo "\n\nYou can now run OpenAllumette with the command allumette. Good game." || \
+        { echo "Failed to run pip3."; exit 4; }
